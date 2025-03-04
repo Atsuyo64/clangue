922a00f0c157c28a2be1cccb90566af8bd5b9610
Atsuyo_ <132666940+Atsuyo64@users.noreply.github.com>
2025-03-04 14:06:54 +0100

    feat: more tests

--- tests/correct-examples/basic.c
+++ tests/correct-examples/basic.c
@@ -1,8 +1,8 @@
-nmain() //not main !
+main() //something
 { int i, j,k ,r;
 i =-3e0 /* comment */;
 j=40e01 ;
-// int y;
+int y;
 const THIs_1s_a_N4Me;
 k=8 ;
 // printf (i) ;
--- tests/correct-examples/huge.c
+++ tests/correct-examples/huge.c
@@ -1,4 +1,4 @@
-int func(int a,int b) {
+/* int func(int a,int b) {
     if(a) {
         if (b)
             printf(b);
@@ -7,17 +7,22 @@ int func(int a,int b) {
         }
     }
     return b;
-}
+}*/
 
 main()
 
 {
     const v,_U = 12;
-    int a =0    ;
-    func(a,55);
+    int b,a =0    ;
+    //func(a,55);
 
     while(a <= 12) {
         a = a<<1;
         a = a & 31; //0b11111 
     }
+    if(a==14)
+        if(a == -2*4e20)
+            a = 1;
+        else
+            a = 2;
 }
