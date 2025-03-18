891c877e704fec1dad628d1b0cd9d7f0dd77d106
Atsuyo_ <132666940+Atsuyo64@users.noreply.github.com>
2025-03-18 14:54:32 +0100

    feat: utils
    Co-authored-by: MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>

---
+++ src/utils.c
@@ -0,0 +1,25 @@
+#include "src/utils.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define DEBUG_VEC
+
+#define MAX_VAR_NAM_SIZE 256
+
+#ifdef DEBUG_VEC
+#include <stdio.h>
+#endif //DEBUG_VEC
+
+static int tmpCnt = 0;
+static char tmpName[MAX_VAR_NAM_SIZE];
+
+char *getTempName()
+{
+    memset(&tmpName[0], 0, MAX_VAR_NAM_SIZE);
+    strcpy(tmpName, "__TMP");
+    char tmpNameNumber[MAX_VAR_NAM_SIZE - 5];
+    sprintf(tmpNameNumber, "%d", tmpCnt);
+    strcat(tmpName, tmpNameNumber);
+    tmpCnt++;
+    return tmpName;
+}
