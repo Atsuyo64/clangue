0a6133f94cec519423ad62c71e5aba5f0550922e
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-05-10 00:45:00 +0200

    feat: more complicated example (eheh the dangling else

---
+++ tests/correct-examples/dangling.c
@@ -0,0 +1,16 @@
+main(){
+    int a = 254;
+    int c = 0;
+    while (a)
+    {
+        if (1)
+            if (a-255) // if a != 255
+                c = 66;
+            else
+                c = 64;
+        
+        a=a+1;
+    }
+    int b=104;
+    b = b + 1;
+}
