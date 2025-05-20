23ccf274d39fd86d7149452ba0e0bc6eadcc7ee2
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-05-20 18:54:03 +0200

    feat: stack overflow

---
+++ tests/correct-examples/hardpointer.c
@@ -0,0 +1,20 @@
+main(){
+    // printf(1,17);
+    int a = 7;
+    int b = 10;
+    int *a_ptr;
+    // printf(1, &a); // 4
+    a_ptr = &a;
+    // printf(1, a_ptr); // 4
+    *a_ptr = 3;
+    a_ptr = a_ptr + 4;
+    *a_ptr = 9; 
+    printf(1,b);
+    // *a_ptr = 9;
+    // int a2 = a_ptr;
+    // int stdout = 1;
+    // printf(1, &a_ptr); // 8
+    // printf(1, a_ptr); // 4
+    // printf(1, *a_ptr); //3
+    // printf(1, a); //3
+}
